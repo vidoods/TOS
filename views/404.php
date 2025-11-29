93550df18166f34758d1bfbdcffe93462f72758b
vidoods <vitalikfromodessa@gmail.com>
2025-11-29 16:01:11 +0200

    Create 404.php

---
+++ views/404.php
@@ -0,0 +1,5 @@
+<div class="container" style="text-align: center; padding: 50px;">
+    <h1>404 - Страница не найдена</h1>
+    <p>К сожалению, запрошенная вами страница не существует.</p>
+    <a href="index.php?view=dashboard" class="btn btn-primary">Перейти на главную</a>
+</div>
