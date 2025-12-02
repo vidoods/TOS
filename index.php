323bb1295084ab5e9f8b71ba5ee0261d779bcf8d
vidoods <vitalikfromodessa@gmail.com>
2025-12-02 14:00:58 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -64,7 +64,7 @@ $currentTitle = $pageTitles[$view] ?? 'TOS - Trading Operating System';
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet">
     
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-    
+    <link href="https://cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet">
     <link rel="stylesheet" href="assets/style.css?v=<?php echo time(); ?>">
 </head>
 <body>
@@ -130,7 +130,7 @@ $currentTitle = $pageTitles[$view] ?? 'TOS - Trading Operating System';
 		<img class="modal-content" id="modal-image">
 		<div id="modal-caption"></div>
 	</div>
-
+	<script src="https://cdn.quilljs.com/1.3.6/quill.js"></script>
     <script src="assets/app.js?v=<?php echo time(); ?>"></script>
 
 </body>
