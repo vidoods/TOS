67e6db040352ffa1366ab1ae12c413913e659945
Your Name <you@example.com>
2026-05-30 13:10:25 +0300

    feat: add favicon links to improve site branding

--- index.php
+++ index.php
@@ -94,6 +94,8 @@ $currentTitle = $pageTitles[$view] ?? 'TradeOS - Trading Operating System';
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
     <link rel="apple-touch-icon" href="assets/logo.png">
+    <link rel="icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
